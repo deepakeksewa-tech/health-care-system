21191e0532423d8b684fbbda77519f947dfebd50
Deepakmahajan482 <Deepakmahajan3316@gmail.com>
2026-07-27 21:34:20 +0530

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", // React/Vite URL
+  origin: "https://health-care-system-seven.vercel.app/", // React/Vite URL
   credentials: true,
 }));
 

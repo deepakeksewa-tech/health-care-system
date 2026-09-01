9d2ff9bf422135e07b5cca91a672ba37bedb735b
deepakeksewa-tech <deepakeksewa@gmail.com>
2026-09-01 12:07:08 +0530

    backend update

--- backend/swagger.js
+++ backend/swagger.js
@@ -3,19 +3,28 @@ import swaggerJsdoc from "swagger-jsdoc";
 const options = {
   definition: {
     openapi: "3.0.0",
+
     info: {
       title: "Doctor Consultancy API",
       version: "1.0.0",
       description: "API documentation for Doctor Consultancy MERN Project",
     },
+
     servers: [
       {
-        url: ["http://localhost:8000","https://health-care-system-vv00.onrender.com"],
+        url: "http://localhost:5000",
+        description: "Local Development Server",
+      },
+      {
+        url: "https://health-care-system-vv00.onrender.com",
+        description: "Production Server",
       },
     ],
   },
-  apis: ["./routes/*.js"], // Change according to your project structure
+
+  apis: ["./routes/*.js"],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
+
 export default swaggerSpec;

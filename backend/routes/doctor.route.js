f2697c6af491be86b3243f2fcca081e30571f7b2
deepakeksewa-tech <deepakeksewa@gmail.com>
2026-09-21 15:53:32 +0530

    doctor dashboard3

--- backend/routes/doctor.route.js
+++ backend/routes/doctor.route.js
@@ -530,7 +530,7 @@ router.post("/category", DoctorMiddleware, category);
  */
 router.get("/ReadCategory", ReadCategory);
 
-router.post("/detuct/money",DoctorMiddleware,detuctMoney)
+router.post("/deduct/money",DoctorMiddleware,detuctMoney)
 router.post("/logout",DoctorMiddleware,logout);
 
 export default router;
